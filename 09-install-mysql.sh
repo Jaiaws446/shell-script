b45a742354c7b1424f54ade546b0c6837ed24d06
jaiaws446 <12192jaipal@gmail.com>
2023-12-23 15:33:51 +0530

    conditions

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,9 +5,10 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: please run this script with root user"
+    exit 1 # you can give other than 0
 else
     echo "you are root user"
-fi
+fi #fi means reverse of if, indicating condition end
 
 yum install mysql -y
 
