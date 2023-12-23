d6ba932fd423e2c9c7ab130e42d2f390f3b88aee
jaiaws446 <12192jaipal@gmail.com>
2023-12-23 11:50:48 +0530

    echo conversation

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,5 +2,5 @@
 
 echo "Ramesh:: hello suresh, good morning"
 echo "Suresh:: Hi Ramesh, very good morning"
-echo: "Ramesh:: how are you"
-echo: "Suresh:: i am good, how are you Ramesh"
+echo "Ramesh:: how are you"
+echo "Suresh:: i am good, how are you Ramesh"
