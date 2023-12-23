fcf7d2fbf1139865180f82bd76460b836219b160
jaiaws446 <12192jaipal@gmail.com>
2023-12-23 12:41:18 +0530

    variables with args

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,10 @@ echo "please enter your username"
 
 read -s USERNAME #the value entered above will be automatically attached USERNAME variable
 
-echo "username is: $USERNAME"
+#echo "username is: $USERNAME" # iam printing just for validation, you shoud not print username and passwords
+
+echo "please enter your password"
+
+read -s PASSWORD
+
+echo "username is: $USERNAME, password is: $PASSWORD"
