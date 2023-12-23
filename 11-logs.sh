c4c5ab82fe143529e89fda93ff7495e008f96cf1
jaiaws446 <12192jaipal@gmail.com>
2023-12-23 18:21:55 +0530

    speacial vars

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,6 +2,9 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%h-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -10,17 +13,17 @@ echo "script name: $0"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-    echo "ERROR:: $2 ...failed"
+    echo -e "ERROR:: $2 ...$R failed $N"
     exit 1
     else
-    echo "$2 ...success"
+    echo -e "$2 ...$G success $N"
     fi
 
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root user"
+    echo -e "$R ERROR:: please run this script with root user $N"
     exit 1 # you can give other than 0
 else
     echo "you are root user"
