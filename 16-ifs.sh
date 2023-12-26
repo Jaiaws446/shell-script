0b6fbf938e5f6ce1df30b4aeabe59a4cf34876e0
jaiaws446 <12192jaipal@gmail.com>
2023-12-26 11:39:43 +0530

    ifs

--- 16-ifs.sh
+++ 16-ifs.sh
@@ -2,3 +2,20 @@
 
 file=/etc/passwd
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ ! -f $file] # ! denotes inversion
+then
+    echo -e "$R Source directory: $file does not exists. $N"
+fi
+
+while IFS=":" read -r username password user_id group_id user_fullname home_dir shell_path
+do
+    echo "username: $username"
+    echo "user ID: $user_id"
+    echo "user full name: $user_fullname"
+
+done < $file
